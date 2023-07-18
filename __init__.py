0179d7f3e3689f2cbcfcb76ab3e722f97944f2b7
ksgv <ksgv@odoo.com>
2023-07-18 16:58:40 -0600

    commit -m creation of init and mainfest

---
+++ __init__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Motorcycle Registry',
+    'summary': 'Manage Registration of Motorcycles',
+    'description': """AttributeErrorMotorcycle Registry 
+    ====================
+    This Module is used to keep track of the Motorcycle Registration and Ownership of each motorcycled of the brand.""",
+    'author': 'ksgv-odoo',
+    'website': 'https://github.com/ksgv-odoo'
+
+}
