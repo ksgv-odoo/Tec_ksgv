bcd0f8c4592a5fc46580fb352601504a0409a3d6
ksgv <ksgv@odoo.com>
2023-07-19 09:32:38 -0600

    reordenamiento de archivos

--- custom_modules/motorcycle_registry/__manifest_.py
+++ custom_modules/motorcycle_registry/__manifest_.py
@@ -5,6 +5,15 @@
     ====================
     This Module is used to keep track of the Motorcycle Registration and Ownership of each motorcycled of the brand.""",
     'author': 'ksgv-odoo',
-    'website': 'https://github.com/ksgv-odoo'
+    'website': 'https://github.com/ksgv-odoo',
+    'category': 'custom_modules/motorcycle registry',
+    'license': 'OPL-1',
 
-}
+    'depends': ['base'],
+    'data':[],
+    'demo': [],
+    'assets': [],
+    'instalable': True,
+    'auto_install': False,
+    'application':True
+}   
